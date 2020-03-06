ffd16065c48db14e5cbceb3fbe1636e440f4d670
Gabriel Volpe <volpegabriel@gmail.com>
2020-03-06 13:24:01 -0300

    Nixifying build

---
+++ shopping-cart.nix
@@ -0,0 +1,16 @@
+{ mkDerivation, aeson, async, base, bytestring, containers, dhall
+, refined, stdenv, text, wreq
+}:
+mkDerivation {
+  pname = "shopping-cart";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    aeson async base bytestring containers dhall refined text wreq
+  ];
+  executableHaskellDepends = [ base ];
+  description = "The Shopping Cart developed in PFP Scala for Haskell";
+  license = stdenv.lib.licenses.asl20;
+}
