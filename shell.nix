32a3e243c40d06e54a7523ae701deb9f162b984d
Gabriel Volpe <volpegabriel@gmail.com>
2020-03-18 12:27:53 +0100

    nix-shell hook to get ghcide working

--- shell.nix
+++ shell.nix
@@ -10,4 +10,10 @@ pkgs.stdenv.mkDerivation {
   buildInputs = project.env.nativeBuildInputs ++ [
     haskellPackages.cabal-install
   ];
+  shellHook = ''
+    export NIX_GHC="$(which ghc)"
+    export NIX_GHCPKG="$(which ghc-pkg)"
+    export NIX_GHC_DOCDIR="$NIX_GHC/../../share/doc/ghc/html"
+    export NIX_GHC_LIBDIR="$(ghc --print-libdir)"
+  '';
 }
