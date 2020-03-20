3cb81f668cc807feaf043e9a94a5f02315301b2a
Gabriel Volpe <volpegabriel@gmail.com>
2020-03-20 13:54:42 +0100

    removing unused imports

--- src/Http/Handler.hs
+++ src/Http/Handler.hs
@@ -1,12 +1,10 @@
 module Http.Handler where
 
 import           Control.Monad.IO.Class         ( liftIO )
-import           Control.Monad.Catch            ( handle )
 import           Domain.Brand
 import           Domain.Item
 import           Http.Params
 import           Servant
-import           Servant.API
 import           Services.Brands                ( Brands )
 import qualified Services.Brands               as SB
 import           Services.Items                 ( Items )
