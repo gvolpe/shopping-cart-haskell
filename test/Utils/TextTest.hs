fe30ad9c5537bbab129c00d044c7d55af16efbe1
Gabriel Volpe <volpegabriel@gmail.com>
2020-03-21 22:15:58 +0100

    utils text test suite passing

--- test/Utils/TextTest.hs
+++ test/Utils/TextTest.hs
@@ -1,20 +1,23 @@
-{-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
-
 {-# LANGUAGE LambdaCase, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, ViewPatterns #-}
 
 module Utils.TextTest where
 
 import           Data.Char                      ( isLower
                                                 , isUpper
                                                 )
+import qualified Data.Text                     as T
 import           Hedgehog
 import qualified Hedgehog.Gen                  as Gen
 import qualified Hedgehog.Range                as Range
+import           Utils.Text                     ( capitalize )
 
 prop_capitalize :: Property
 prop_capitalize = property $ do
-  (forAll $ Gen.list (Range.linear 0 100) Gen.alpha) >>= \case
-    (x : xs) -> (isUpper x && all isLower xs) === True
+  (forAll $ (capitalize . T.pack) <$> Gen.list (Range.linear 0 100) Gen.alpha)
+    >>= \case
+          (T.unpack -> x : xs) -> isUpper x === all isLower xs
+          (T.unpack -> _     ) -> pure ()
 
 utilTextTests :: Group
 utilTextTests = $$(discover)
