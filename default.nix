af0c0369aded435f469761588996d058f58ac352
Gabriel Volpe <volpegabriel@gmail.com>
2020-04-23 22:38:31 +0200

    simplifying nix shell

--- default.nix
+++
@@ -1,2 +0,0 @@
-{ nixpkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/1fe82110feb.tar.gz") {}, compiler ? "ghc865" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./shopping-cart.nix { }
--- shell.nix
+++ shell.nix
@@ -1,20 +1,20 @@
-{ nixpkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/1fe82110feb.tar.gz") {} }:
 let
-  inherit (nixpkgs) pkgs;
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs-channels/archive/1fe82110feb.tar.gz") {};
   inherit (pkgs) haskellPackages;
+  drv = haskellPackages.callCabal2nix "shopping-cart" ./. {};
 
-  project = import ./release.nix;
 in
-pkgs.stdenv.mkDerivation {
-  name = "shell";
-  buildInputs = project.env.nativeBuildInputs ++ [
-    haskellPackages.cabal-install
-    haskellPackages.hlint
-  ];
-  shellHook = ''
-    export NIX_GHC="$(which ghc)"
-    export NIX_GHCPKG="$(which ghc-pkg)"
-    export NIX_GHC_DOCDIR="$NIX_GHC/../../share/doc/ghc/html"
-    export NIX_GHC_LIBDIR="$(ghc --print-libdir)"
-  '';
-}
+  {
+    my_project = drv;
+    shell = haskellPackages.shellFor {
+      withHoogle = true;
+      packages = p: [drv];
+      buildInputs = with haskellPackages; [ hlint cabal-install ];
+      shellHook = ''
+        export NIX_GHC="$(which ghc)"
+        export NIX_GHCPKG="$(which ghc-pkg)"
+        export NIX_GHC_DOCDIR="$NIX_GHC/../../share/doc/ghc/html"
+        export NIX_GHC_LIBDIR="$(ghc --print-libdir)"
+      '';
+    };
+  }.shell
