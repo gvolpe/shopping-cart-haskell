d2a3ce6856fb1fb73db8fa3dbb6f8f42179e80ad
Gabriel Volpe <volpegabriel@gmail.com>
2021-07-19 17:03:11 +0200

    Fix tests

--- test/Payments/Test.hs
+++ test/Payments/Test.hs
@@ -21,17 +21,17 @@ prop_payments = withTests (1 :: TestLimit) payments
 
 payments :: Property
 payments = property $ do
-  client <- evalIO P.mkPaymentClient
   evalIO $ putStrLn "Making payment request"
   uuid <- evalIO UUID.nextRandom
   pid  <- evalIO $ P.processPayment client (payment uuid)
   (Just pid) === (PaymentId <$> fromText "6ff33dbc-6b8c-11ea-bc55-0242ac130003")
  where
-  cn   = $$(refineTH "Gabriel Volpe") :: CardNamePred
-  cu   = $$(refineTH 1111444477772222) :: CardNumberPred
-  ce   = $$(refineTH 1204) :: CardExpirationPred
-  cv   = $$(refineTH 123) :: CardCVVPred
-  card = Card (CardName cn) (CardNumber cu) (CardExpiration ce) (CardCVV cv)
+  client = P.mkPaymentClient
+  cn     = $$(refineTH "Gabriel Volpe") :: CardNamePred
+  cu     = $$(refineTH 1111444477772222) :: CardNumberPred
+  ce     = $$(refineTH 1204) :: CardExpirationPred
+  cv     = $$(refineTH 123) :: CardCVVPred
+  card   = Card (CardName cn) (CardNumber cu) (CardExpiration ce) (CardCVV cv)
   payment uid = Payment (UserId uid) (Money 500) card
 
 paymentTests :: Group
